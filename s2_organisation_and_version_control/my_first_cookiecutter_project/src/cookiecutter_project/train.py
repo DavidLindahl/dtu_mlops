0d203bc3f87dd6da927cebe96c114c880af7e932
David Lindahl <s234817@dtu.dk>
2026-01-06 15:14:13 +0100

    make own ML project using nickis project template

---
+++ s2_organisation_and_version_control/my_first_cookiecutter_project/src/cookiecutter_project/train.py
@@ -0,0 +1,10 @@
+from cookiecutter_project.model import Model
+from cookiecutter_project.data import MyDataset
+
+def train():
+    dataset = MyDataset("data/raw")
+    model = Model()
+    # add rest of your training code here
+
+if __name__ == "__main__":
+    train()
